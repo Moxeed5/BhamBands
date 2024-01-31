f65bf54e8bc386f1c2661ce6988b7c44a4ae518f
Max Morgan <maxmmorgan93@gmail.com>
2024-01-30 22:37:43 -0600

    initial commit, setting up MVC application for finding bands in Bham

---
+++ Models/Event.cs
@@ -0,0 +1,14 @@
+﻿namespace BhamBands.Models
+{
+    public class Event
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        // ... other properties ...
+
+        // Navigation properties
+        public Venue Venue { get; set; }
+        public List<TicketPrice> TicketPrices { get; set; }
+    }
+
+}
---
+++ Models/Venue.cs
@@ -0,0 +1,13 @@
+﻿namespace BhamBands.Models
+{
+    public class Venue
+    {
+        public int VenueId { get; set; }
+        public string Name { get; set; }
+        // ... other properties ...
+
+        // Navigation back to Event
+        public List<Event> Events { get; set; }
+    }
+
+}
